9973af7f0fdd82470da5c3db2371b13b377b7069
Jordan Maynor <mail@jtm.li>
2024-09-30 14:03:50 -0500

    Switch to logger and batch notification format

--- src/main.py
+++ src/main.py
@@ -2,9 +2,28 @@
 Main module for the project.
 """
 
+import atexit
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from src.logger import AppriseNotifier, error_handler, logger
+
+
+@atexit.register
+def send_error_summary():
+    """
+    Send out a summary of any errors that occurred during
+    program run to defined apprise endpoints.
+    """
+    errors = error_handler.get_errors()
+    if errors:
+        error_summary = "\n".join(errors)
+        AppriseNotifier().error_notify(
+            f"Program exited with the following errors:\n{error_summary}"
+        )
+
 
 if __name__ == "__main__":
     pass
